ed1a5b767bd50effa8eb326415b94cc9d3cb8dd1
SKT-310 <156750500+SKT-310@users.noreply.github.com>
2024-09-13 07:13:13 +0200

    Added Practise files

---
+++ Day-08/test.py
@@ -0,0 +1,6 @@
+s3_bucket_lists = ["shiva_demo_bucket","ramu_demo_bucket"]
+s3_bucket_lists.append ("New_s3_bucket")
+
+print(s3_bucket_lists)
+new_list = s3_bucket_lists [0:2]
+print(new_list)
---
+++ Day-09/test.py
@@ -0,0 +1,5 @@
+numbers = [1,2,3,4,5]
+for shiva in numbers:
+    if shiva == 3:
+       continue
+    print (shiva)
